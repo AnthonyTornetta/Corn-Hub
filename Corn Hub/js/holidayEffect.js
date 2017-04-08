31f5039db962b174a5663c32b6a462bf70a2dfdb
Cornchipss <cornchipgonecodin@gmail.com>
2017-04-07 21:46:10 -0400

    V1.003
    Did a long-needed update

---
+++ Corn Hub/js/holidayEffect.js
@@ -0,0 +1,18 @@
+/*var d = new Date();
+var day = d.getDate(); // Starts at 1
+var month = d.getMonth() + 1; // Starts at 0
+
+var xmas = day >= 25 && day <= 31 && month == 12; // Xmas
+var stPatty = day == 5 && month == 4;
+var color = "#FFF";
+
+if (xmas) // Xmas
+{
+    window.location.replace("xmas.html");
+}
+if (stPatty)
+{
+    color = "green";
+    document.write("<div id='snowflakeContainer'> <p style='color: " + color + "' class='snowflake'>&#9752;</p></div>'");
+}
+*/
