488482b1310d8e38751de1ca52343ce20423c30d
BuildTools <cornchipgonecodin@gmail.com>
2017-04-15 00:24:16 -0400

    V1.007
    Touched up a few more things

--- Corn Hub/js/events.js
+++ Corn Hub/js/events.js
@@ -1,48 +1,8 @@
-// JSON from https://learnwebcode.github.io/json-example/animals-1.json
-/*
-/*
-var container = document.getElementById("animal-info");
-var url = "https://learnwebcode.github.io/json-example/animals-1.json";
-var request = new XMLHttpRequest();
-request.open("GET", url);
-request.onload = function ()
-{
-    var data = JSON.parse(request.responseText);
-    console.log(data[0]);
-    for(i = 0; i < data.length; i++)
-    {
-        renderHTML(data, i);
-    }
-};
-request.send();
-
-function renderHTML(data, i)
-{
-    var htmlString = '<p>' + data[i].name + ' is a ' + data[i].species + ' and likes ';
-    for(j = 0; j < data[i].foods.likes.length; j++)
-    {
-        if(j == 0)
-            htmlString += data[i].foods.likes[j];
-        else
-            htmlString += ' and ' + data[i].foods.likes[j];
-    }
-    
-    for(j = 0; j < data[i].foods.dislikes.length; j++)
-    {
-        if(j == 0)
-            htmlString += data[i].foods.dislikes[j];
-        else
-            htmlString += ' and ' + data[i].foods.dislikes[j];
-    }
-    htmlString + '.</p>';
-    
-    container.insertAdjacentHTML('beforeend', htmlString);
-}
-*/
 var container = document.getElementById("news");
+var source = "the-washington-post";
 var API = 'ba7c0aede1d140d7b36a2cccd893e665';
 var url = 'https://newsapi.org/v1/articles?source=[src]&sortBy=top&apiKey=' + API;
-url = url.replace('[src]', 'the-wall-street-journal');
+url = url.replace('[src]', source);
 var request = new XMLHttpRequest();
 request.open("GET", url);
 request.onload = function ()
