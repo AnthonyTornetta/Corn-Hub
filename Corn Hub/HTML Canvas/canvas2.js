bf4fb267af3d375fb11e6349874c1c123da0bd97
Cornchip <cornchipgonecodin@gmail.com>
2017-09-24 20:49:12 -0400

    Puzzah
    Puzzzaahhh

--- Corn Hub/HTML Canvas/canvas2.js
+++ Corn Hub/HTML Canvas/canvas2.js
@@ -15,7 +15,7 @@ ctx = canvas.getContext("2d");
 w = canvas.width  = window.innerWidth;
 h = canvas.height = window.innerHeight;
 
-logo.src = "img.png";
+logo.src = "img.jpg";
 
 logo.onload = function()
 {
