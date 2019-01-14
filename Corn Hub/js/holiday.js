26166c1ecfdfb672cb919576b2a17042724ba052
Cornchipss <cornchipgonecodin@gmail.com>
2019-01-14 08:37:24 -0500

    Custom holiday support

--- Corn Hub/js/holiday.js
+++ Corn Hub/js/holiday.js
@@ -1,22 +1,50 @@
 let url = "https://www.checkiday.com/api.php?d=";
 $(() =>
 {
-	updateHolidays(url);
+	let customHolidays = {};
+	let today = new Date();
 
-  function updateHolidays(url)
+	function index(d)
 	{
-		$.getJSON(url, function(holidayData)
-		{
-			let container = document.getElementById('checkiday');
-
-			container.innerHTML = '<ul class="checkiday" id="checkiday-list"></ul>';
-			for(let i = 0; i < holidayData.length; i++)
-			{
-				let ul = document.getElementById('checkiday-list');
-				let li = document.createElement('li');
-				li.appendChild(document.createTextNode(holidayData[i]));
-				ul.appendChild(li);
-			}
-		});
+		return d.getMonth() * 100 + d.getDate();
 	}
+
+	function addHoliday(month, day, name)
+	{
+		let holidayIndex = month * 100 + day;
+		if(customHolidays[holidayIndex])
+			customHolidays[holidayIndex] = [];
+		
+		customHolidays[holidayIndex].push(name);
+	}
+
+	addHoliday(0, 14, "Testing Holiday :D");
+	addHoliday(0, 13, "ur dumb");
+
+	function appendHoliday(holiday)
+	{
+		let ul = document.getElementById('checkiday-list');
+		let li = document.createElement('li');
+		li.appendChild(document.createTextNode(holiday));
+		ul.appendChild(li);
+	}
+
+	$.getJSON(url, function(holidayData)
+	{
+		let container = document.getElementById('checkiday');
+
+		container.innerHTML = '<ul class="checkiday" id="checkiday-list"></ul>';
+		for(let i = 0; i < holidayData.length; i++)
+		{
+			appendHoliday(holidayData[i]);
+		}
+
+		if(customHolidays[index(today)])
+		{
+			customHolidays[index(today)].forEach(holiday =>
+			{
+				appendHoliday(holiday);
+			});
+		}
+	});
 });
